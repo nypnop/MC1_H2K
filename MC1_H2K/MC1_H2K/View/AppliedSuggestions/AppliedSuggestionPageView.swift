5555396d62bad219a258c013b193f0f06ef90294
Bayu Alif Farisqi <bayualiev@gmail.com>
2023-05-04 10:45:06 +0700

    Applied Suggestion Final

--- MC1_H2K/MC1_H2K/View/AppliedSuggestions/AppliedSuggestionPageView.swift
+++ MC1_H2K/MC1_H2K/View/AppliedSuggestions/AppliedSuggestionPageView.swift
@@ -10,6 +10,12 @@ import SwiftUI
 struct AppliedSuggestionPageView: View {
     @State var styleName : String = "Fearful-Avoidant"
     @State var imageName : String = "Avatar-FA"
+    @State var cards = [
+        ("Title 1", "List", "Description 1","ConversationStarter"),
+        ("Title 2", "Telphone Sekarang", "Description 2","whatsapp://"),
+        ("Title 3", "Pesan Gojek Food", "Description 3","gojek://"),
+        ("Title 3", "Pesan Grab Food", "Description 3","grab://"),
+    ]
     var body: some View {
         NavigationView{
             ScrollView{
@@ -19,11 +25,20 @@ struct AppliedSuggestionPageView: View {
                             .font(.caption)
                             .foregroundColor(Color("GrayLight"))
                         Spacer()
-                    }
+                    }.padding(.horizontal, 24)
+                        .padding(.bottom)
                     ComparisonStyle(styleName: $styleName, imageName: $imageName)
-                }.padding(.horizontal, 16)
-                .navigationBarTitle("Applied Suggestion")
-                .edgesIgnoringSafeArea(.bottom)
+                        .padding(.horizontal, 24)
+                        .padding(.bottom)
+                    VStack {
+                        ForEach(cards.indices, id: \.self) { index in
+                            SuggestionCard(CardTitle: $cards[index].0, CardAction: $cards[index].1, CardDescription: $cards[index].2, CardLink: $cards[index].3)
+                        }
+                        
+                    }
+                }
+                    .navigationBarTitle("Applied Suggestion")
+                    .edgesIgnoringSafeArea(.bottom)
             }
         }
     }
